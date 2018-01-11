e59c6786ad312d2786b7b891898894c9165218b2
Mike Campbell <mike@exactsolutions.biz>
2018-01-11 15:26:21 -0600

    formatting changes to variable functions

--- PublicFolderMigration.psm1
+++ PublicFolderMigration.psm1
@@ -2,49 +2,54 @@
 #Module Variables and Variable Functions
 ###############################################################################################
 function Get-PFMVariable
-{
-param
-(
-[string]$Name
-)
-    Get-Variable -Scope Script -Name $name
-}
+    {
+        param
+        (
+        [string]$Name
+        )
+            Get-Variable -Scope Script -Name $name
+    }
+#end function Get-PFMVariable
 function Get-PFMVariableValue
-{
-param
-(
-[string]$Name
-)
-    Get-Variable -Scope Script -Name $name -ValueOnly
-}
+    {
+        param
+        (
+            [string]$Name
+        )
+            Get-Variable -Scope Script -Name $name -ValueOnly
+    }
+#end function Get-PFMVariableValue
 function Set-PFMVariable
-{
-param
-(
-[string]$Name
-,
-$Value
-)
-    Set-Variable -Scope Script -Name $Name -Value $value
-}
+    {
+        param
+        (
+            [string]$Name
+            ,
+            $Value
+        )
+        Set-Variable -Scope Script -Name $Name -Value $value
+    }
+#end function Set-PFMVariable    
 function New-PFMVariable
-{
-param
-(
-[string]$Name
-,
-$Value
-)
-    New-Variable -Scope Script -Name $name -Value $Value
-}
+    {
+        param
+        (
+            [string]$Name
+            ,
+            $Value
+        )
+        New-Variable -Scope Script -Name $name -Value $Value
+    }
+#end function New-PFMVariable
 function Remove-PFMVariable
-{
-param
-(
-[string]$Name
-)
-    Remove-Variable -Scope Script -Name $name
-}
+    {
+        param
+        (
+            [string]$Name
+        )
+        Remove-Variable -Scope Script -Name $name
+    }
+#end function Remove-PFMVariable
 ###############################################################################################
 #Core Public Folder Migration Module Functions
 ###############################################################################################
