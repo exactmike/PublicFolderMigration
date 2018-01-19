01a9fdb9bc34542cf2eaa4bcf757388c1d6635ee
exactmike <mike@exactsolutions.biz>
2018-01-19 10:57:33 -0500

    import some *.ps1 files in the main module file

--- PublicFolderMigration.psm1
+++ PublicFolderMigration.psm1
@@ -1,6 +1,10 @@
 ###############################################################################################
 #Core Public Folder Migration Module Functions
 ###############################################################################################
+. $(Join-Path $PSScriptRoot 'PublicFunctions.ps1')
+. $(Join-Path $PSScriptRoot 'SupportingFunctions.ps1')
+. $(Join-Path $PSScriptRoot 'ThirdPartyFunctions.ps1')
+. $(Join-Path $PSScriptRoot 'ModuleVariableFunctions.ps1')
 
 function Find-OrphanedMailEnabledPublicFolders
     {
