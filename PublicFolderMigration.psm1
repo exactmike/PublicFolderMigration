ced0e0dc0f541ae0cbafdcf6f84e533b572fe002
exactmike <mike@exactsolutions.biz>
2018-01-22 21:45:11 -0500

    Add import of PrivateFunctions.ps1 to the main module script

--- PublicFolderMigration.psm1
+++ PublicFolderMigration.psm1
@@ -3,6 +3,7 @@
 ###############################################################################################
 $script:ConnectExchangeOrganizationCompleted = $false
 . $(Join-Path $PSScriptRoot 'PublicFunctions.ps1')
+. $(Join-Path $PSScriptRoot 'PrivateFunctions.ps1')
 . $(Join-Path $PSScriptRoot 'SupportingFunctions.ps1')
 . $(Join-Path $PSScriptRoot 'ThirdPartyFunctions.ps1')
 . $(Join-Path $PSScriptRoot 'ModuleVariableFunctions.ps1')
