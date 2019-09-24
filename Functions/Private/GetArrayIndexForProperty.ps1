082b87e76c5c08c47878bbead8a686e667efa96b
MIke Campbell <mike@exactsolutions.biz>
2019-09-24 19:27:28 -0400

    update GetArrayIndexForProperty Function

--- Functions/Private/GetArrayIndexForProperty.ps1
+++ Functions/Private/GetArrayIndexForProperty.ps1
@@ -1,10 +1,11 @@
-Function GetArrayIndexForIdentity
+Function GetArrayIndexForProperty
 {
-
     [cmdletbinding()]
     [OutputType([System.Int32])]
     param(
         [parameter(mandatory = $true)]
+        [AllowNull()]
+        [AllowEmptyCollection()]
         $array #The array for which you want to find a value's index
         ,
         [parameter(mandatory = $true)]
@@ -14,6 +15,13 @@ Function GetArrayIndexForIdentity
         $property #The property name for the value for which you want to find an index
     )
     Write-Verbose -Message 'Using Property Match for Index'
-    [array]::indexof(($array.$property).guid, $value)
+    if ($null -ne $array -and $array.count -ne 0)
+    {
+        [array]::indexof(($array.$property), $value)
+    }
+    else
+    {
+        -1
+    }
 
 }
