c0f95c130d853ef1519463460a81c9d99e0ad9cb
MIke Campbell <mike@exactsolutions.biz>
2019-09-24 19:29:26 -0400

    Module removal cleanup code added

--- PublicFolderMigration.psm1
+++ PublicFolderMigration.psm1
@@ -21,3 +21,20 @@ $AllFunctionFiles = Get-ChildItem -Recurse -File -Path $(Join-Path -Path $PSScri
 #$PrivateFunctionFiles = $AllFunctionFiles.where( { $_.PSParentPath -like '*\Private' })
 foreach ($ff in $AllFunctionFiles) { . $ff.fullname }
 #Export-ModuleMember -Function $PublicFunctionFiles.foreach( { $_.BaseName })
+
+
+###############################################################################################
+#Public Folder Migration Module Removal Routines
+###############################################################################################
+#Clean up objects that will exist in the Global Scope due to no fault of our own . . .
+$MyInvocation.MyCommand.ScriptBlock.Module.OnRemove =
+{
+    if ($null -ne $Script:PSSession) {Remove-PSSession -Session $script:Pssession}
+    if ($null -ne $script:ParallelPSSession)
+    {
+        foreach ($session in $script:ParallelPSSession)
+        {
+            Remove-PSSession -Session $session
+        }
+    }
+}
