16a99f3e1533524f271abeaf08c364028dbb321b
MIke Campbell <mike@exactsolutions.biz>
2019-11-13 14:40:17 -0500

    more updated help for the command design for invoke-PFMEmptyPublicFolderRemove

--- Functions/Public/InProgress/Invoke-PFMEmptyPublicFolderRemove.ps1
+++ Functions/Public/InProgress/Invoke-PFMEmptyPublicFolderRemove.ps1
@@ -4,19 +4,21 @@
     .DESCRIPTION
     Accepts one or more EntryIDs or other Public Folder Unique Identifiers and processes them for removal if they meet the required validations.
     .PARAMETER PublicFolderMailboxServer
-    This parameter specifies the Exchange server from which to retrieve folder information to generate the Public Folder Information Objects.
+    Use to  specifies the Exchange server from which to retrieve folder information to generate the Public Folder Information Objects.
     .PARAMETER Identity
-    This parameter specifies the identity(ies) of the public folder(s) to be validated for and processed for removal
+    Use to specify the identity(ies) of the public folder(s) to be validated for and processed for removal.
+    .PARAMETER Validations
+    Use to specify the validations to run before processing a public folder for removal. NoSubfolders,NotMailEnabled,NoItems
     .PARAMETER Passthru
-    Controls whether the public folder validation objects are returned to the PowerShell pipeline for further processing.
+    Controls whether the public folder validation/removal result objects are returned to the PowerShell pipeline for further processing.
     .PARAMETER OutputFolderPath
     Mandatory parameter for the already existing directory location where you want public folder replication and stats reports to be placed.  Operational log files will also go to this location.
     .PARAMETER OutputFormat
     Mandatory parameter used to specify whether you want csv, json, xml, clixml or any combination of these.
     .EXAMPLE
     Connect-PFMExchange -ExchangeOnPremisesServer USCLTEX10PF01.us.clt.contoso.com -credential $cred
-    Get-PFMPublicFolderTree -OutputFolderPath c:\PFReports -OutputFormats csv,json,xml -PublicFolderMailboxServer USCLTEX10PF01
+    Invoke-PFMEmptyPublicFolderRemoval
 
     If public folders are on Exchange 2010, the ExchangeOnPremisesServer must be an Exchange 2010 server.
     Gets public folder tree data from USCLTEX10PF01.us.clt.contoso.com and exports it to csv, json, and xml formats in c:\PFReports
-    #>
+#>
