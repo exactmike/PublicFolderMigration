8c168fedd209265d7325c20f32dd179f775cd256
Mike Campbell <mike@campbellnetwork.net>
2019-11-15 10:58:52 -0500

    change function for more testing

--- Functions/Public/InProgress/Remove-PFMValidatedPublicFolder.ps1
+++ Functions/Public/InProgress/Remove-PFMValidatedPublicFolder.ps1
@@ -3,6 +3,7 @@ Function Remove-PFMValidatedPublicFolder
     [cmdletbinding()]
     [OutputType([pscustomobject])]
     param(
+        [parameter(Mandatory, ValueFromPipeline)]
         [PSTypeName("PublicFolderValidation")]$PublicFolderValidation
     )
     $PublicFolderValidation
