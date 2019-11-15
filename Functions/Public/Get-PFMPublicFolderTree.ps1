6495d532fbf0cc089a86b43da77015bf6f4f1b2e
Mike Campbell <mike@campbellnetwork.net>
2019-11-15 00:47:41 -0500

    move public folder  property list into a module variable PublicFolderPropertyList

--- Functions/Public/Get-PFMPublicFolderTree.ps1
+++ Functions/Public/Get-PFMPublicFolderTree.ps1
@@ -85,41 +85,13 @@ Function Get-PFMPublicFolderTree
         Default
         { 'Root' }
     }
-    writelog -Message "PublicFolder Path Type specified by user parameters: $PublicFolderPathType"  -EntryType Notification -verbose
+    WriteLog -Message "PublicFolder Path Type specified by user parameters: $PublicFolderPathType"  -EntryType Notification -verbose
     #endregion ValidateParameters
     #region BuildServerAndDatabaseLists
     WriteLog -Message "Validated Public Folder Mailbox Server To Query: $PublicFolderMailboxServer" -EntryType Notification -Verbose
 
     #setup property list to retrieve
-    $PropertyList = @(
-        @{n = 'EntryID'; e = { $_.EntryID.tostring() } }
-        'Name'
-        @{n = 'Identity'; e = { $_.Identity.tostring() } }
-        @{n = 'MapiIdentity'; e = { $_.MapiIdentity.tostring() } }
-        'ParentPath'
-        'HasSubFolders'
-        @{n = 'ReplicasString'; e = { $_.Replicas -join ';' } }
-        'Replicas'
-        @{n = 'ReplicaCount'; e = { $_.Replicas.count } }
-        'UseDatabaseReplicationSchedule'
-        @{n = 'ReplicationScheduleString'; e = { $_.ReplicationSchedule -join ';' } }
-        'ReplicationSchedule'
-        'PerUserReadStateEnabled'
-        'FolderType'
-        'MailEnabled'
-        'HiddenFromAddressListsEnabled'
-        'MaxItemSize'
-        'UseDatabaseQuotaDefaults'
-        'IssueWarningQuota'
-        'ProhibitPostQuota'
-        'UseDatabaseRetentionDefaults'
-        'RetainDeletedItemsFor'
-        'UseDatabaseAgeDefaults'
-        'AgeLimit'
-        'HasRules'
-        'HasModerator'
-        'IsValid'
-    )
+    $PropertyList = $Script:PublicFolderPropertyList
 
     $GetPublicFolderParams = @{ }
     if ($Recurse)
