7d282fa6b12b9d2844115edce907bc676015d411
Mike Campbell <mike@campbellnetwork.net>
2019-11-15 11:55:30 -0500

    prepare remove-pfmvpf for more testing

--- Functions/Public/InProgress/Remove-PFMValidatedPublicFolder.ps1
+++ Functions/Public/InProgress/Remove-PFMValidatedPublicFolder.ps1
@@ -5,12 +5,68 @@ Function Remove-PFMValidatedPublicFolder
     param(
         [parameter(Mandatory, ValueFromPipeline)]
         [PSTypeName("PublicFolderValidation")]$PublicFolderValidation
+        ,
+        [parameter(Mandatory)]
+        [ValidateScript( { TestIsWriteableDirectory -path $_ })]
+        [string]$OutputFolderPath
+        ,
+        [parameter()]
+        [ValidateSet('Unicode', 'BigEndianUnicode', 'Ascii', 'Default', 'UTF8', 'UTF8NOBOM', 'UTF7', 'UTF32')]
+        [string]$Encoding = 'UTF8'
     )
+    begin
+    {
+        Connect-PFMExchange -IsParallel -ExchangeOnPremisesServer $script:ExchangeOnPremisesServer
+        $ParallelSession = Get-PFMParallelPSSession -name $Script:ExchangeOnPremisesServer
+        $BeginTimeStamp = Get-Date -Format yyyyMMdd-HHmmss
+        $ResultPath = Join-Path -path $OutputFolderPath -ChildPath $($BeginTimeStamp + 'RemoveValidatedPublicFolder.log')
+        $ErrorResultPath = Join-Path -path $OutputFolderPath -ChildPath $($BeginTimeStamp + 'RemoveValidatedPublicFolder-ERRORS.log')
+    }
     process
     {
         foreach ($pfv in $PublicFolderValidation)
         {
-            $pfv
+            $haderror = $false
+            if ($true -eq $pfv.Validated)
+            {
+                $pfv.ActionName = 'Remove'
+                $RemovePFParams = @{
+                    ErrorAction = 'Stop'
+                    Confirm     = $false
+                    Whatif      = $True
+                    Identity    = $pfv.FoundEntryID
+                }
+                try
+                {
+                    Invoke-Command -Session $ParallelSession -ScriptBlock {
+                        Remove-PublicFolder @using:RemovePFParams
+                    } -ErrorAction 'Stop'
+                    $pfv.ActionResult = $true
+                }
+                catch
+                {
+                    $myerrorstring = $_.tostring()
+                    $haderror = $true
+                    $pfv.ActionResult = $false
+                    $pfv.ActionError = $myerrorstring
+                }
+                $pfv.ActionTimeStamp = Get-Date -Format yyyyMMdd-HHmmss
+            }
+            else
+            {
+                $pfv.ActionName = 'None'
+                $pfv.ActionTimeStamp = Get-Date -Format yyyyMMdd-HHmmss
+            }
+            $pfvjson = ConvertTo-Json -InputObject $pfv -Compress
+            Out-File -InputObject $pfvjson -Append -FilePath $ResultPath -Encoding $Encoding
+            if ($true -eq $haderror)
+            {
+                Out-File -InputObject $pfvjson -Append -FilePath $ErrorResultPath -Encoding $Encoding
+            }
         }
+    }
+    end
+    {
+
     }
 }
