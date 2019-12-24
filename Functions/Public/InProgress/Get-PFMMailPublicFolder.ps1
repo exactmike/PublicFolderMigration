e400663ae0920179764b1f55e387f3058e7b912b
Mike Campbell <mike@exactsolutions.biz>
2019-12-23 20:43:16 -0500

    update get-pfmmailpublicfolder for 'standalone' branch and add parameter set for mapping PF to MEPF.

--- Functions/Public/InProgress/Get-PFMMailPublicFolder.ps1
+++ Functions/Public/InProgress/Get-PFMMailPublicFolder.ps1
@@ -1,28 +1,63 @@
 Function Get-PFMMailPublicFolder
 {
-    [cmdletbinding(DefaultParameterSetName = 'All')]
+    [cmdletbinding(DefaultParameterSetName = 'RecipientOnly')]
     [OutputType([System.Object[]])]
     param(
+        [parameter(Mandatory, ValueFromPipeline, ParameterSetName = 'TreeMappedRecipient')]
+        [psobject[]]$PublicFolderInfoObject
     )
-    #Get all mail enabled public folders
-    $getMailPublicFolderParams = @{
-        ResultSize    = 'Unlimited'
-        ErrorAction   = 'stop'
-        WarningAction = 'SilentlyContinue'
-    }
-    try
+    begin
     {
-        $message = "Get All Mail Enabled Public Folder Objects"
-        Write-Information -MessageData $message -Tags Attempts
-        Get-MailPublicFolder @getMailPublicFolderParams
-        Write-Information -MessageData $message -Tags Successes
+        Confirm-PFMExchangeConnection -PSSession $Script:PSSession
+        $BeginTimeStamp = Get-Date -Format yyyyMMdd-HHmmss
+        $script:LogPath = Join-Path -path $OutputFolderPath -ChildPath $($BeginTimeStamp + 'GetMailPublicFolder.log')
+        $script:ErrorLogPath = Join-Path -path $OutputFolderPath -ChildPath $($BeginTimeStamp + 'GetMailPublicFolder-ERRORS.log')
+        #$Stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
+        WriteLog -Message "Calling Invocation = $($MyInvocation.Line)" -EntryType Notification
+        WriteLog -Message "Exchange Session is Running in Exchange Organzation $script:ExchangeOrganization" -EntryType Notification
 
     }
-    catch
+    process
     {
-        $myerror = $_
-        Write-Information -MessageData $message -Tags Errors
-        Write-Information -MessageData $myerror.tostring() -Tags Errors
-        $myerror
+        Switch ($PSCmdlet.ParameterSetName)
+        {
+            'RecipientOnly'
+            {
+                Invoke-Command -ErrorAction Stop -WarningAction SilentlyContinue -Session $script:PSSession -ScriptBlock {
+                    Get-MailPublicFolder -ResultSize Unlimited -ErrorAction Stop
+                }
+            }
+            'TreeMappedRecipient'
+            {
+                foreach ($pf in $PublicFolderInfoObject)
+                {
+                    $CurrentPF++
+                    $GetMailPublicFolderParams = @{
+                        Identity      = $pf.Identity.tostring()
+                        ErrorAction   = 'SilentlyContinue'
+                        WarningAction = 'SilentlyContinue'
+                    }
+                    $Status = "Get-MailPublicFolder -Identity $($pf.Identity.tostring())"
+                    Write-Progress -Activity $message -Status $Status -CurrentOperation "$CurrentPF of $($PublicFolderInfoObject.Count)" -PercentComplete $($CurrentPF / $PublicFolderInfoObject.Count * 100)
+                    try
+                    {
+                        #output Selected object with additional properties from the Pf object
+                        $MEPF = Invoke-Command -Session $script:PSSession -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -ScriptBlock {
+                            Get-MailPublicFolder @using:GetMailPublicFolderParams
+                        }
+                        if ($null -ne $MEPF)
+                        {
+                            $MEPF | Select-Object -Property *, @{n = 'EntryID'; e = { $pf.EntryID.tostring() } }, @{n = 'PFIdentity'; e = { $pf.Identity.tostring() } }
+                        }
+                    }
+                    catch
+                    {
+                        $myerror = $_
+                        WriteLog -message $Status -EntryType Failed
+                        WriteLog -message $myerror.tostring() -ErrorLog
+                    }
+                }
+            }
+        }
     }
 }
