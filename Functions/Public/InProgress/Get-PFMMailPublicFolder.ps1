e243493eedf7626e00471f18fb91efb8aa866da6
Mike Campbell <mike@exactsolutions.biz>
2019-12-23 21:25:19 -0500

    updates to public and ready Get-PFMMailPublicFolder

--- Functions/Public/InProgress/Get-PFMMailPublicFolder.ps1
+++ Functions/Public/InProgress/Get-PFMMailPublicFolder.ps1
@@ -3,22 +3,32 @@ Function Get-PFMMailPublicFolder
     [cmdletbinding(DefaultParameterSetName = 'RecipientOnly')]
     [OutputType([System.Object[]])]
     param(
-        [parameter(Mandatory, ValueFromPipeline, ParameterSetName = 'TreeMappedRecipient')]
+        [parameter(Mandatory, ParameterSetName = 'TreeMappedRecipient')]
+        #PublicFolderInfoObjects to scope the retrieval of Mail Public Folders to the submitted objects.  Also includes EntryID and Identity in the resulting output objects.
         [psobject[]]$PublicFolderInfoObject
+        ,
+        [Parameter(Mandatory)]
+        [ValidateScript( { TestIsWriteableDirectory -Path $_ })]
+        $OutputFolderPath
+        ,
+        [parameter(Mandatory)]
+        [ExportDataOutputFormat[]]$Outputformat
+        ,
+        [parameter()]
+        [ValidateSet('Unicode', 'BigEndianUnicode', 'Ascii', 'Default', 'UTF8', 'UTF8NOBOM', 'UTF7', 'UTF32')]
+        [string]$Encoding = 'UTF8'
+        ,
+        [parameter()]
+        [switch]$Passthru
     )
-    begin
-    {
-        Confirm-PFMExchangeConnection -PSSession $Script:PSSession
-        $BeginTimeStamp = Get-Date -Format yyyyMMdd-HHmmss
-        $script:LogPath = Join-Path -path $OutputFolderPath -ChildPath $($BeginTimeStamp + 'GetMailPublicFolder.log')
-        $script:ErrorLogPath = Join-Path -path $OutputFolderPath -ChildPath $($BeginTimeStamp + 'GetMailPublicFolder-ERRORS.log')
-        #$Stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
-        WriteLog -Message "Calling Invocation = $($MyInvocation.Line)" -EntryType Notification
-        WriteLog -Message "Exchange Session is Running in Exchange Organzation $script:ExchangeOrganization" -EntryType Notification
-
-    }
-    process
-    {
+    Confirm-PFMExchangeConnection -PSSession $Script:PSSession
+    $BeginTimeStamp = Get-Date -Format yyyyMMdd-HHmmss
+    $script:LogPath = Join-Path -path $OutputFolderPath -ChildPath $($BeginTimeStamp + 'GetMailPublicFolder.log')
+    $script:ErrorLogPath = Join-Path -path $OutputFolderPath -ChildPath $($BeginTimeStamp + 'GetMailPublicFolder-ERRORS.log')
+    #$Stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
+    WriteLog -Message "Calling Invocation = $($MyInvocation.Line)" -EntryType Notification
+    WriteLog -Message "Exchange Session is Running in Exchange Organzation $script:ExchangeOrganization" -EntryType Notification
+    $Results = @(
         Switch ($PSCmdlet.ParameterSetName)
         {
             'RecipientOnly'
@@ -59,5 +69,18 @@ Function Get-PFMMailPublicFolder
                 }
             }
         }
+    )
+    $ResultCount = $Results.Count
+    WriteLog -Message "Count of Mail Enabled PublicFolders Retrieved: $ResultCount" -EntryType Notification -verbose
+    $CreatedFilePath = @(
+        foreach ($of in $Outputformat)
+        {
+            Export-PFMData -ExportFolderPath $OutputFolderPath -DataToExportTitle 'MailEnabledPublicFolders' -ReturnExportFilePath -Encoding $Encoding -DataFormat $of -DataToExport $Results
+        }
+    )
+    WriteLog -Message "Output files created: $($CreatedFilePath -join '; ')" -entryType Notification -verbose
+    if ($true -eq $Passthru)
+    {
+        $Results
     }
 }
